3a50a36792f4199f956e09abeabfadbc4689e164
rlaal <rlaalgus132@naver.com>
2020-10-10 01:52:55 +0900

    addRetrofit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants{
+
+    companion object{
+        const val API_KEY="6d81b3744ece4ea6b41c1ac596625571"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+
+}
